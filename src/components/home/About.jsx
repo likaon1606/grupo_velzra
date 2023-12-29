bdacea1780a7e4e6e584e550c5f185e808101df8
Ariel Fuentes <ariel.fuentesgarcia@gmail.com>
2023-12-29 09:31:58 -0600

    service

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -5,15 +5,15 @@ const About = () => {
   return (
     <>
       {' '}
-      <div className="video-container">
-        <video className="video" loop autoPlay muted>
-          <source src="./assets/construccion.mp4" />
+      <div className='video-container'>
+        <video className='video' loop autoPlay muted>
+          <source src='./assets/construccion.mp4' />
         </video>
       </div>
-      <div className="container-body">
-        <div className="body-text">
-          <h1 className="body-t">¿Quiénes Somos?</h1>
-          <p className="body-p">
+      <div className='container-body'>
+        <div className='body-text'>
+          <h1 className='body-t'>¿Quiénes Somos?</h1>
+          <p className='body-p'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
             maiores ad porro fugit iure, consequatur inventore ea harum,
             perspiciatis accusamus, optio repudiandae natus a doloribus culpa
--- src/components/servicio/Service.jsx
+++ src/components/servicio/Service.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import '../../styles/service.css';
 
 const Service = () => {
-  return <div style={{ color: 'white' }}>Service</div>;
+  return (
+    <div className='container-service'>
+      <h1>SERVICIOS</h1>
+      <div className='columns-container'>
+        <div className='column'>
+          <img src='./assets/construccion.jpg' alt='Imagen 1' />
+          <p>
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quis
+            aliquam, deserunt eligendi facere id, repellendus ex reiciendis
+            inventore perferendis unde iusto sed ab doloribus. Sunt officia
+            quidem amet in, debitis harum aut ratione delectus blanditiis
+            aliquam est laborum nihil deleniti, molestias nisi voluptate dolorum
+            inventore nostrum maiores nam aspernatur. Nam, eaque sunt culpa
+            iusto cum dolorum, ullam dolores quod laudantium rem blanditiis aut?
+            Unde vel laudantium iusto porro aut.{' '}
+          </p>
+        </div>
+        <div className='column'>
+          <img src='./assets/construccion.jpg' alt='Imagen 2' />
+          <p>
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quis
+            aliquam, deserunt eligendi facere id, repellendus ex reiciendis
+            inventore perferendis unde iusto sed ab doloribus. Sunt officia
+            quidem amet in, debitis harum aut ratione delectus blanditiis
+            aliquam est laborum nihil deleniti, molestias nisi voluptate dolorum
+            inventore nostrum maiores nam aspernatur. Nam, eaque sunt culpa
+            iusto cum dolorum, ullam dolores quod laudantium rem blanditiis aut?
+            Unde vel laudantium iusto porro aut.{' '}
+          </p>
+        </div>
+        <div className='column'>
+          <img src='./assets/construccion.jpg' alt='Imagen 3' />
+          <p>
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quis
+            aliquam, deserunt eligendi facere id, repellendus ex reiciendis
+            inventore perferendis unde iusto sed ab doloribus. Sunt officia
+            quidem amet in, debitis harum aut ratione delectus blanditiis
+            aliquam est laborum nihil deleniti, molestias nisi voluptate dolorum
+            inventore nostrum maiores nam aspernatur. Nam, eaque sunt culpa
+            iusto cum dolorum, ullam dolores quod laudantium rem blanditiis aut?
+            Unde vel laudantium iusto porro aut.{' '}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
