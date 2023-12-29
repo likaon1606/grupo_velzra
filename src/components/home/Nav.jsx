4443e0a1ed435c9cb0334daea3a71340b3e71fa5
Ariel Fuentes <ariel.fuentesgarcia@gmail.com>
2023-12-29 11:13:55 -0600

    color active

--- src/components/home/Nav.jsx
+++ src/components/home/Nav.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../../styles/nav.css';
 
 const Nav = () => {
   return (
-    <div className="container-nav">
-      <div className="cont-info">
+    <div className='container-nav'>
+      <div className='cont-info'>
         <div>
           <ul>
             <a
-              className="icon-img"
-              href="https://api.whatsapp.com/send/?phone=5641815041"
-              target="blank"
+              className='icon-img'
+              href='https://api.whatsapp.com/send/?phone=5641815041'
+              target='blank'
             >
               <li>
-                <img className="icons" src="./assets/whats.png" alt="celular" />
+                <img className='icons' src='./assets/whats.png' alt='celular' />
               </li>
             </a>
-            <a className="icon-img" href="mailto:ventas@grupovelzra.com.mx">
+            <a className='icon-img' href='mailto:ventas@grupovelzra.com.mx'>
               <li>
-                <img className="icons" src="./assets/mail.png" alt="mail" />
+                <img className='icons' src='./assets/mail.png' alt='mail' />
               </li>
             </a>
           </ul>
@@ -27,42 +27,42 @@ const Nav = () => {
         <div>
           <ul>
             <a
-              className="icon-img"
-              href="https://www.linkedin.com/company/3804283/admin/feed/posts/"
-              target="blank"
+              className='icon-img'
+              href='https://www.linkedin.com/company/3804283/admin/feed/posts/'
+              target='blank'
             >
               <li>
                 <img
-                  className="icons"
-                  src="./assets/linkedin.png"
-                  alt="linkedin"
+                  className='icons'
+                  src='./assets/linkedin.png'
+                  alt='linkedin'
                 />
               </li>
             </a>
             <a
-              className="icon-img"
-              href="https://www.youtube.com/@grupovelzraholding"
-              target="blank"
+              className='icon-img'
+              href='https://www.youtube.com/@grupovelzraholding'
+              target='blank'
             >
               <li>
                 <img
-                  className="icons"
-                  src="./assets/youtube.png"
-                  alt="youtube"
+                  className='icons'
+                  src='./assets/youtube.png'
+                  alt='youtube'
                 />
               </li>
             </a>
 
             <a
-              className="icon-img"
-              href="https://www.instagram.com/grupovelzra/"
-              target="blank"
+              className='icon-img'
+              href='https://www.instagram.com/grupovelzra/'
+              target='blank'
             >
               <li>
                 <img
-                  className="icons"
-                  src="./assets/insta.png"
-                  alt="instagram"
+                  className='icons'
+                  src='./assets/insta.png'
+                  alt='instagram'
                 />
               </li>
             </a>
@@ -70,26 +70,29 @@ const Nav = () => {
         </div>
       </div>
 
-      <div className="nav">
-        <div className="logo">
+      <div className='nav'>
+        <div className='logo'>
           {' '}
-          <img src="./assets/logo.png" alt="logo" />
+          <img src='./assets/logo.png' alt='logo' />
         </div>
 
         <div>
-          <ul className="menu">
-            <Link to="/">
-              <li className="menu-li">INICIO</li>
-            </Link>
-            <Link to="/modelodenegocio">
-              <li className="menu-li">MODELO DE NEGOCIO</li>
-            </Link>
-            <Link to="/servicio">
-              <li className="menu-li">SERVICIO </li>
-            </Link>
-            <Link to="/contacto">
-              <li className="menu-li">CONTACTO</li>
-            </Link>
+          <ul className='menu'>
+            <li className='menu-li'>
+              <NavLink exact to='/'>
+                INICIO
+              </NavLink>
+            </li>
+
+            <li className='menu-li'>
+              <NavLink to='/modelodenegocio'>MODELO DE NEGOCIO</NavLink>
+            </li>
+            <li className='menu-li'>
+              <NavLink to='/servicio'>SERVICIO</NavLink>
+            </li>
+            <li className='menu-li'>
+              <NavLink to='/contacto'>CONTACTO</NavLink>
+            </li>
           </ul>
         </div>
       </div>
