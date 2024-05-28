3b117e1cb7e1b7d16c89bc33ad6b75f4e5932f8f
Marketing3 <ariel.fuentesgarcia@gmail.com>
2024-05-28 08:39:29 -0600

    corrections img static

---
+++ src/components/carousel_clients/CarouselClients.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CarouselClients = () => {
+  return <div>CarouselClients</div>;
+};
+
+export default CarouselClients;
--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../styles/about.css';
 import Carousel from '../carousel/Carousel';
+import CarouselTop from '../carousel_top/CarouselTop';
+import CarouselClients from '../carousel_clients/CarouselClients';
 
 import video from '../../assets/velzra.mp4';
 import velzra from '../../assets/velzra.png';
@@ -14,7 +16,6 @@ import cons4 from '../../assets/cons-4.png';
 import mision from '../../assets/mision.png';
 import vision from '../../assets/vision.png';
 import timeline from '../../assets/timeline.png';
-import CarouselTop from '../carousel_top/CarouselTop';
 
 const About = () => {
   return (
@@ -191,101 +192,116 @@ const About = () => {
         </p>
       </div>
       <hr />
-      <div className='container-static'>
-        <div className='img-static1'>
-          <div className='cont-txt-img'>
-            <h1>Acabados</h1>
-            <div className='cont-ul'>
-              <ul>
-                <li>Pinturas Vinílicas</li>
-                <li>Pintura de Esmalte</li>
-                <li>Pasta Acrílica</li>
-                <li>Texturizados y efectos</li>
-                <li>Pintura tipo pizarrón (Sketch)</li>
-                <li>Muros de Yeso y Cemento</li>
-                <li>Plafones prefabricados</li>
-                <li>Rotulación</li>
-              </ul>
+      <div className='container-imgs-stc'>
+        <div className='imgs-static'>
+          <div className='img-static1'>
+            <div className='cont-txt-img'>
+              <h1>Acabados</h1>
+              <div className='cont-ul'>
+                <ul>
+                  <li>Pinturas Vinílicas</li>
+                  <li>Pintura de Esmalte</li>
+                  <li>Pasta Acrílica</li>
+                  <li>Texturizados y efectos</li>
+                  <li>Pintura tipo pizarrón (Sketch)</li>
+                  <li>Muros de Yeso y Cemento</li>
+                  <li>Plafones prefabricados</li>
+                  <li>Rotulación</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='container-static'>
-        <div className='img-static2'>
-          <div className='cont-txt-img'>
-            <h1>Impermeabilizantes</h1>
-            <div className='cont-ul'>
-              <ul>
-                <li>Acrílico</li>
-                <li>Asfaltos en Frío</li>
-                <li>Cementosos</li>
-                <li>Prefabricados</li>
-                <li>Dinámicos de alto desempeño</li>
-                <li>Aislantes térmicos</li>
-              </ul>
+      <div className='container-imgs-stc'>
+        <div className='imgs-static'>
+          <div className='img-static2'>
+            <div className='cont-txt-img'>
+              <h1>Impermeabilizantes</h1>
+              <div className='cont-ul'>
+                <ul>
+                  <li>Acrílico</li>
+                  <li>Asfaltos en Frío</li>
+                  <li>Cementosos</li>
+                  <li>Prefabricados</li>
+                  <li>Dinámicos de alto desempeño</li>
+                  <li>Aislantes térmicos</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='container-static'>
-        <div className='img-static3'>
-          <div className='cont-txt-img'>
-            <h1>Recubrimientos Industriales</h1>
-            <div className='cont-ul'>
-              <ul>
-                <li>Alquidálicos</li>
-                <li>Acrílicos</li>
-                <li>Epóxicos</li>
-                <li>Poliuretanos</li>
-                <li>Vinílicos</li>
-                <li>Polisiloxanos</li>
-                <li>Orgánicos / Inorgánicos de Zinc</li>
-              </ul>
+      <div className='container-imgs-stc'>
+        <div className='imgs-static'>
+          <div className='img-static3'>
+            <div className='cont-txt-img'>
+              <h1>Recubrimientos Industriales</h1>
+              <div className='cont-ul'>
+                <ul>
+                  <li>Alquidálicos</li>
+                  <li>Acrílicos</li>
+                  <li>Epóxicos</li>
+                  <li>Poliuretanos</li>
+                  <li>Vinílicos</li>
+                  <li>Polisiloxanos</li>
+                  <li>Orgánicos / Inorgánicos de Zinc</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='container-static'>
-        <div className='img-static4'>
-          <div className='cont-txt-img'>
-            <h1>Especialidad en Pisos</h1>
-            <div className='cont-ul'>
-              <ul>
-                <li>Acabado todo tipo de pintura</li>
-                <li>Acabado tipo carpeta</li>
-                <li>Piso a base de mortero</li>
-                <li>Pisos decorativos</li>
-                <li>Piso terrazo</li>
-                <li>Piso con sembrado de cuarzo</li>
-                <li>Piso Deconkret</li>
-              </ul>
+      <div className='container-imgs-stc'>
+        <div className='imgs-static'>
+          <div className='img-static4'>
+            <div className='cont-txt-img'>
+              <h1>Especialidad en Pisos</h1>
+              <div className='cont-ul'>
+                <ul>
+                  <li>Acabado todo tipo de pintura</li>
+                  <li>Acabado tipo carpeta</li>
+                  <li>Piso a base de mortero</li>
+                  <li>Pisos decorativos</li>
+                  <li>Piso terrazo</li>
+                  <li>Piso con sembrado de cuarzo</li>
+                  <li>Piso Deconkret</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='container-static'>
-        <div className='img-static5'>
-          <div className='cont-txt-img'>
-            <h1>Productos Especializados</h1>
-            <div className='cont-ul'>
-              <ul>
-                <li>Impermeabilizantes</li>
-                <li>Pinturas Vinílicas y esmaltes</li>
-                <li>Anticorrosivos</li>
-                <li>Pinturas Industriales</li>
-                <li>Cementos antiácidos y membranas</li>
-                <li>Retardantes de fuego</li>
-                <li>Pintura tráfico y señalización</li>
-                <li>Pisos decorativos y de alto desempeño</li>
-                <li>Pintura en polvo</li>
-                <li>Pintura de proceso</li>
-                <li>Sistemas de construcción ligera</li>
-                <li>Recubrimientos de alta temperatura</li>
-              </ul>
+      <div className='container-imgs-stc'>
+        <div className='imgs-static'>
+          <div className='img-static5'>
+            <div className='cont-txt-img'>
+              <h1>Productos Especializados</h1>
+              <div className='cont-ul'>
+                <ul>
+                  <li>Impermeabilizantes</li>
+                  <li>Pinturas Vinílicas y esmaltes</li>
+                  <li>Anticorrosivos</li>
+                  <li>Pinturas Industriales</li>
+                  <li>Cementos antiácidos y membranas</li>
+                  <li>Retardantes de fuego</li>
+                  <li>Pintura tráfico y señalización</li>
+                  <li>Pisos decorativos y de alto desempeño</li>
+                  <li>Pintura en polvo</li>
+                  <li>Pintura de proceso</li>
+                  <li>Sistemas de construcción ligera</li>
+                  <li>Recubrimientos de alta temperatura</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <hr />
+      <div className='container-clients'>
+        <h1>Nuestros Clientes</h1>
+      </div>
+      <CarouselClients />
     </>
   );
 };
