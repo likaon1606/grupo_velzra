72ba5a515bc8031cfeb262400f7291ee2d81301e
Marketing3 <ariel.fuentesgarcia@gmail.com>
2024-05-29 17:49:13 -0600

    carousel clients

--- src/components/carousel_clients/CarouselClients.jsx
+++ src/components/carousel_clients/CarouselClients.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import slideImages from './slideImages';
+import './carouselClients.css';
 
 const CarouselClients = () => {
-  return <div>CarouselClients</div>;
+  return (
+    <Carousel
+      showArrows={true}
+      showStatus={false}
+      showIndicators={false}
+      showThumbs={false}
+      autoPlay
+      infiniteLoop
+      centerMode
+      centerSlidePercentage={33.33}
+      interval={2000}
+    >
+      {slideImages.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt={`Slide ${index + 1}`} />
+        </div>
+      ))}
+    </Carousel>
+  );
 };
 
 export default CarouselClients;
---
+++ src/components/carousel_clients/slideImages.js
@@ -0,0 +1,40 @@
+import slide_image_1 from '../../assets/CLIENTES/bbb.png';
+import slide_image_2 from '../../assets/CLIENTES/carvid.png';
+import slide_image_3 from '../../assets/CLIENTES/cfe.png';
+import slide_image_4 from '../../assets/CLIENTES/cm.png';
+import slide_image_5 from '../../assets/CLIENTES/conaculta.png';
+import slide_image_6 from '../../assets/CLIENTES/fa.png';
+import slide_image_7 from '../../assets/CLIENTES/fia.png';
+import slide_image_8 from '../../assets/CLIENTES/for.png';
+import slide_image_9 from '../../assets/CLIENTES/generac.png';
+import slide_image_10 from '../../assets/CLIENTES/ine.png';
+import slide_image_11 from '../../assets/CLIENTES/infonavit.png';
+import slide_image_12 from '../../assets/CLIENTES/isem.png';
+import slide_image_13 from '../../assets/CLIENTES/issste.png';
+import slide_image_14 from '../../assets/CLIENTES/ocesa.png';
+import slide_image_15 from '../../assets/CLIENTES/sedena.png';
+import slide_image_16 from '../../assets/CLIENTES/semar.png';
+import slide_image_17 from '../../assets/CLIENTES/wal.png';
+
+const slideImages = [
+  slide_image_1,
+  slide_image_2,
+  slide_image_3,
+  slide_image_4,
+  slide_image_5,
+  slide_image_6,
+  slide_image_7,
+  slide_image_8,
+  slide_image_9,
+  slide_image_10,
+  slide_image_11,
+  slide_image_12,
+  slide_image_12,
+  slide_image_13,
+  slide_image_14,
+  slide_image_15,
+  slide_image_16,
+  slide_image_17,
+];
+
+export default slideImages;
