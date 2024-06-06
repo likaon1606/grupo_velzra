bb8aec649f303d21684ba5050429ec7994d985c8
Marketing3 <ariel.fuentesgarcia@gmail.com>
2024-06-06 11:59:23 -0600

    projects finish

--- src/components/proyectos/projects_img.js
+++ src/components/proyectos/projects_img.js
@@ -1,14 +1,14 @@
-import img_1 from '../../assets/imgs/3B CEDIS1.jpg';
-import img_2 from '../../assets/imgs/Autodromo.jpg';
-import img_3 from '../../assets/imgs/CANCHAS deportivas.jpg';
-import img_4 from '../../assets/imgs/Camino 3B.jpg';
-import img_5 from '../../assets/imgs/F1 linea deportiva.png';
-import img_6 from '../../assets/imgs/ISSTE Merida.jpg';
-import img_7 from '../../assets/imgs/PIRELLI.png';
+import img_1 from '../../assets/imgs/ISSTE Merida.jpg';
+import img_2 from '../../assets/imgs/google.jpg';
+import img_3 from '../../assets/imgs/formula_e.jpg';
+import img_4 from '../../assets/imgs/cosmopol.jpg';
+import img_5 from '../../assets/imgs/CANCHAS deportivas.jpg';
+import img_6 from '../../assets/imgs/Camino 3B.jpg';
+import img_7 from '../../assets/imgs/Autodromo.jpg';
 import img_8 from '../../assets/imgs/areopuerto.jpg';
-import img_9 from '../../assets/imgs/cosmopol.jpg';
-import img_10 from '../../assets/imgs/formula_e.jpg';
-import img_11 from '../../assets/imgs/google.jpg';
+import img_9 from '../../assets/imgs/3B CEDIS1.jpg';
+import img_10 from '../../assets/imgs/PIRELLI.png';
+import img_11 from '../../assets/imgs/F1 linea deportiva.png';
 
 const imgs = [
   img_1,
